a106ef9085f3a67b1e047a0f170f9966cfb7aacf
neorselas <126072709+neorselas@users.noreply.github.com>
2023-04-02 23:31:55 +0800

    initial

---
+++ cart.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+	var productItem = [{productName: "CHIPS",price: "20.00",photo: "chips.jpg"},
+		{productName: "BREADS",price: "30.00",photo: "breads.jpg"},
+		{productName: "SODAS",price: "25.00",photo: "sodas.jpg"},
+		{productName: "NOODLES",price: "50.00",photo: "noodles.jpg"},
+		{productName: "SHAMPOO",price: "80.00",photo: "shampoo.jpg"},
+		{productName: "CANNED GOODS",price: "25.00",photo: "canned goods.jpg"},
+		{productName: "LIQUORS",price: "150.00",photo: "liquors.jpg"},
+		{productName: "BATH SOAP",price: "60.00",photo: "soap.jpg"}];
+
+	showProductGallery(productItem);
+	showCartTable();
+});
+
+function addToCart(element) {
+	var productParent = $(element).closest('div.product-item');
+
+	var price = $(productParent).find('.price span').text();
+	var productName = $(productParent).find('.productname').text();
+	var quantity = $(productParent).find('.product-quantity').val();
+
+	var cartItem = {
+		productName: productName,
+		price: price,
+		quantity: quantity
+	};
+	var cartItemJSON = JSON.stringify(cartItem);
+
+	var cartArray = new Array();
+	// If javascript shopping cart session is not empty
+	if (sessionStorage.getItem('shopping-cart')) {
+		cartArray = JSON.parse(sessionStorage.getItem('shopping-cart'));
+	}
+	cartArray.push(cartItemJSON);
+
+	var cartJSON = JSON.stringify(cartArray);
+	sessionStorage.setItem('shopping-cart', cartJSON);
+	showCartTable();
+}
+
+function emptyCart() {
+	if (sessionStorage.getItem('shopping-cart')) {
+		// Clear JavaScript sessionStorage by index
+		sessionStorage.removeItem('shopping-cart');
+		showCartTable();
+	}
+}
+
+function removeCartItem(index) {
+	if (sessionStorage.getItem('shopping-cart')) {
+		var shoppingCart = JSON.parse(sessionStorage.getItem('shopping-cart'));
+		sessionStorage.removeItem(shoppingCart[index]);
+		showCartTable();
+	}
+}
+
+function showCartTable() {
+	var cartRowHTML = "";
+	var itemCount = 0;
+	var grandTotal = 0;
+
+	var price = 0;
+	var quantity = 0;
+	var subTotal = 0;
+
+	if (sessionStorage.getItem('shopping-cart')) {
+		var shoppingCart = JSON.parse(sessionStorage.getItem('shopping-cart'));
+		itemCount = shoppingCart.length;
+
+		//Iterate javascript shopping cart array
+		shoppingCart.forEach(function(item) {
+			var cartItem = JSON.parse(item);
+			price = parseFloat(cartItem.price);
+			quantity = parseInt(cartItem.quantity);
+			subTotal = price * quantity
+
+			cartRowHTML += "<tr>" +
+				"<td>" + cartItem.productName + "</td>" +
+				"<td class='text-right'>" + price.toFixed(2) + "</td>" +
+				"<td class='text-right'>" + quantity + "</td>" +
+				"<td class='text-right'>" + subTotal.toFixed(2) + "</td>" +
+				"</tr>";
+
+			grandTotal += subTotal;
+		});
+	}
+
+	$('#cartTableBody').html(cartRowHTML);
+	$('#itemCount').text(itemCount);
+	$('#totalAmount').text( + grandTotal.toFixed(2));
+}
+
+
+function showProductGallery(product) {
+	//Iterate javascript shopping cart array
+	var productHTML = "";
+	product.forEach(function(item) {
+		productHTML += '<div class="product-item">'+
+					'<img style="width: 150px; height:120px;" src="product-images/' + item.photo + '">'+
+					'<div class="productname">' + item.productName + '</div>'+
+					'<div class="price">Php <span>' + item.price + '</span></div>'+
+					'<div class="cart-action">'+
+						'<input type="text" class="product-quantity" name="quantity" value="1" size="2" />'+
+						'<input type="submit" value="Add to Cart" class="add-to-cart" onClick="addToCart(this)" />'+
+					'</div>'+
+				'</div>';
+				"<tr>";
+		
+	});
+	$('#product-item-container').html(productHTML);
+}
